490b5df6a9f0d0e675bb7ce2f4d8d1515cc14c1a
u295897 <mike.craig@L3Harris.com>
2024-07-11 15:21:45 -0400

    Initial commit

---
+++ Header Files/Currency.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Currency{
+    public: // What everything needs to access
+        string name = "";
+        string ticket = "";
+
+        float cost = 0;
+        int volume_24hr = 0; // Volume ($) traded within last 24 hours
+        int total_supply = 0; // Number of coins on the market
+        int max_supply = 0; // Number of coins that can be produced
+        float market_cap = total_supply * cost; // Total value of current ciculating crypto
+        
+        int one_day_high = 0;
+        int one_day_low = 0;
+        int seven_day_high = 0;
+        int seven_day_low = 0;
+        int month_high = 0;
+        int month_low = 0;
+        int max_high = 0;
+        int max_low = 0;
+
+        //int chatter_score = 0; // Will be added later, how much people are talking about a currency
+    private: // What only the Currency class needs to access
+        string get_name();
+        string get_ticket();
+        vector<double> get_currency_data();
+};
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CryptoTrader
-Automated Crypto Currency Trading Bot Using Arduino to run 24/7
+Automated Crypto Currency Trading Bots Using Arduino to run 24/7. Written in C++ to Integrate w/ Arduino Uno
+
+# Low Risk Day Trader Bot
+    - Picks from a personally select list of proven Altcoins
+    - Follows a simple set of rules to ensure relatively consistent profits automatically:
+        *
